ff826593847f149814f6fc82796e9a2cb66d74ca
Mohsen Rajaei <mrajaie1@gmail.com>
2019-03-31 11:11:58 +0430

    After removing some bug in 'generate_pairs.py', this code working correctly.

--- generate_pairs.py
+++ generate_pairs.py
@@ -43,23 +43,14 @@ class GeneratePairs:
         self._generate_matches_pairs()
         self._generate_mismatches_pairs()
 
-
     def _generate_matches_pairs(self):
         """
         Generate all matches pairs
         """
-        # for name in os.listdir(self.data_dir):
         for name in self.remaining:
-            # if name == ".DS_Store":
-            #     continue
-            # if os.path.isdir(os.path.join(self.data_dir, name)):
             a = []
             for file in os.listdir(os.path.join(self.data_dir, name)):
-            # for file in os.listdir(self.data_dir):
-            #     if file == ".DS_Store":
-            #         continue
                 if self.img_ext in file:
-                    # a.append(file)
                     a.append(os.path.join(name, file))
 
             if a:
@@ -68,9 +59,7 @@ class GeneratePairs:
                         temp = random.choice(a).split(self.separator)  # This line may vary depending on how your images are named.
                         w = self.separator.join(temp[:-1])
 
-                        # l = random.choice(a).split(self.separator)[-1].lstrip("0").rstrip(self.img_ext)
                         l = random.choice(a).split(self.separator)[-1]
-                        # r = random.choice(a).split(self.separator)[-1].lstrip("0").rstrip(self.img_ext)
                         r = random.choice(a).split(self.separator)[-1]
 
                         print("For '" + os.path.join(self.data_dir, name) + "' and counter: ", self.counter, ', Match Pair:', w + " -> " + l
@@ -78,41 +67,25 @@ class GeneratePairs:
 
                         f.write(w + "\t" + l + "\t" + r + "\n")
                         self.counter += 1
-                        # w = temp[0] + "_" + temp[1]
-                        # w = '_'.join(temp[:-1])
-                        # try:
-                        #     l = random.choice(a).split("_")[-1].lstrip("0").rstrip(self.img_ext)
-                        #     r = random.choice(a).split("_")[-1].lstrip("0").rstrip(self.img_ext)
-                        #
-                        #     print(w + " " + l + " " + r, 'counter: ', self.counter)
-                        #
-                        #     if 'William_Ford' in a:
-                        #         print()
-                        #
-                        #
-                        #     f.write(w + "\t" + l + "\t" + r + "\n")
-                        #     self.counter += 1
-                        # except Exception as err:
-                        #     print(err)
-                        #     # print('Setting index to 1')
-                        #     # l = random.choice(a).split("_")[1].lstrip("0").rstrip(self.img_ext)
-                        #     # l = random.choice(a).split.lstrip("0").rstrip(self.img_ext)
-                        #     # r = random.choice(a).split("_")[1].lstrip("0").rstrip(self.img_ext)
-                        #     # f.write(w + "\t" + l + "\t" + r + "\n")
+
 
     def _generate_mismatches_pairs(self):
         """
         Generate all mismatches pairs
         """
-        # for i, name in enumerate(os.listdir(self.data_dir)):
+        tmptmp = ['00061', '10285', '00074', '10156', '10041', '20344', '10041', '20344', '10041', '20344', '10217', '20345', '20324', '20345', '20344',
+                  '10268', '20345', '20481', '20394', '00074', '20412', '10014', '20436', '20412', '30604', '10218']
         for i, name in enumerate(self.remaining):
-                # remaining = os.listdir(self.data_dir)
                 self.update_remaining()
-                # remaining = [f_n for f_n in remaining if f_n != ".DS_Store"]
                 del self.remaining[i]  # deletes the file from the list, so that it is not chosen again
                 other_dir = random.choice(self.remaining)
                 with open(self.pairs_filepath, "a") as f:
                     for _ in range(self.num_random_images_per_folder):
+
+                        if name in tmptmp:
+                            print()
+                        if other_dir in tmptmp:
+                            print()
 
                         temps_file_1 = os.listdir(os.path.join(self.data_dir, name))
                         if temps_file_1:
@@ -122,7 +95,7 @@ class GeneratePairs:
                         if temps_file_2:
                             file2 = random.choice(temps_file_2)
 
-                        if file1 and file2:
+                        if temps_file_1 and temps_file_2:
                             if self.img_ext in file1 and self.img_ext in file2:
                                 print("For '" + self.data_dir + "' and counter: ", self.counter, ', MisMatch Pair:',
                                       name + " " + file1.split(self.separator)[-1] + ' ' +
