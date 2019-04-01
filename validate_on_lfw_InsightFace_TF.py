df82a81c9a299ad640c2ffc9b81bd99b855b9301
Mohsen Rajaei <mrajaie1@gmail.com>
2019-04-01 15:11:21 +0430

    Successfully evaluate custom dataset with pre-trained model by 'InsightFace_TF' evaluation code

--- validate_on_lfw_InsightFace_TF.py
+++ validate_on_lfw_InsightFace_TF.py
@@ -31,9 +31,6 @@ from __future__ import print_function
 
 import tensorflow as tf
 import numpy as np
-import argparse
-# import facenet
-# import lfw
 import os
 import sys
 from tensorflow.python.ops import data_flow_ops
@@ -110,11 +107,6 @@ def main(args):
         with tf.Session() as sess:
             
             # Read the file containing the pairs used for testing
-            pairs = lfw.read_pairs_2(os.path.expanduser(args.lfw_pairs))
-
-            # Get the paths for the corresponding images
-            paths, actual_issame = lfw.get_paths_2(os.path.expanduser(args.lfw_dir), pairs)
-
             # InsightFace_TF evaluation
             ver_list = []
             ver_name_list = []
@@ -123,23 +115,15 @@ def main(args):
             data_set = eval_data_reader.load_eval_datasets_2(args)
             ver_list.append(data_set)
             ver_name_list.append(args.eval_dataset)
+
             #  --------------------------------------------------------------------------------------
 
-            image_paths_placeholder = tf.placeholder(tf.string, shape=(None,1), name='image_paths')
-            labels_placeholder = tf.placeholder(tf.int32, shape=(None,1), name='labels')
-            batch_size_placeholder = tf.placeholder(tf.int32, name='batch_size')
-            control_placeholder = tf.placeholder(tf.int32, shape=(None,1), name='control')
             phase_train_placeholder = tf.placeholder(tf.bool, name='phase_train')
- 
-            nrof_preprocess_threads = 4
-            image_size = (args.image_size, args.image_size)
-            eval_input_queue = data_flow_ops.FIFOQueue(capacity=2000000,
-                                        dtypes=[tf.string, tf.int32, tf.int32],
-                                        shapes=[(1,), (1,), (1,)],
-                                        shared_name=None, name=None)
-            eval_enqueue_op = eval_input_queue.enqueue_many([image_paths_placeholder, labels_placeholder, control_placeholder], name='eval_enqueue_op')
-            image_batch, label_batch = facenet.create_input_pipeline(eval_input_queue, image_size, nrof_preprocess_threads, batch_size_placeholder)
-     
+
+
+            image_batch = tf.placeholder(name='img_inputs', shape=[None, args.image_size, args.image_size, 3], dtype=tf.float32)
+            label_batch = tf.placeholder(name='img_labels', shape=[None, ], dtype=tf.int32)
+
             # Load the model
             input_map = {'image_batch': image_batch, 'label_batch': label_batch, 'phase_train': phase_train_placeholder}
             facenet.load_model(args.model, input_map=input_map)
@@ -147,87 +131,12 @@ def main(args):
             # Get output tensor
             embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
 
-            coord = tf.train.Coordinator()
-            tf.train.start_queue_runners(coord=coord, sess=sess)
-
             results = verification.ver_test(ver_list=ver_list, ver_name_list=ver_name_list, nbatch=0, sess=sess,
                                embedding_tensor=embeddings, batch_size=args.batch_size, feed_dict=input_map,
                                input_placeholder=image_batch, phase_train_placeholder=phase_train_placeholder)
 
             print(results)
 
-            # evaluate(sess, eval_enqueue_op, image_paths_placeholder, labels_placeholder, phase_train_placeholder, batch_size_placeholder,
-            #          control_placeholder, embeddings, label_batch, paths, actual_issame, args.lfw_batch_size, args.lfw_nrof_folds,
-            #          args.distance_metric, args.subtract_mean, args.use_flipped_images, args.use_fixed_image_standardization)
-
-              
-def evaluate(sess, enqueue_op, image_paths_placeholder, labels_placeholder, phase_train_placeholder, batch_size_placeholder,
-             control_placeholder,embeddings, labels, image_paths, actual_issame, batch_size, nrof_folds, distance_metric, subtract_mean,
-             use_flipped_images,use_fixed_image_standardization):
-
-    # Run forward pass to calculate embeddings
-    print('Runnning forward pass on LFW images')
-    
-    # Enqueue one epoch of image paths and labels
-    nrof_embeddings = len(actual_issame)*2  # nrof_pairs * nrof_images_per_pair
-    nrof_flips = 2 if use_flipped_images else 1
-    nrof_images = nrof_embeddings * nrof_flips
-    labels_array = np.expand_dims(np.arange(0,nrof_images),1)
-    image_paths_array = np.expand_dims(np.repeat(np.array(image_paths),nrof_flips),1)
-    control_array = np.zeros_like(labels_array, np.int32)
-    if use_fixed_image_standardization:
-        control_array += np.ones_like(labels_array)*facenet.FIXED_STANDARDIZATION
-    if use_flipped_images:
-        # Flip every second image
-        control_array += (labels_array % 2)*facenet.FLIP
-    sess.run(enqueue_op, {image_paths_placeholder: image_paths_array, labels_placeholder: labels_array, control_placeholder: control_array})
-    
-    embedding_size = int(embeddings.get_shape()[1])
-    print(f"nrof_images: {nrof_images}")
-
-    batch_size = closest_number(nrof_images, batch_size, 100)
-
-    assert nrof_images % batch_size == 0, 'The number of LFW images must be an integer multiple of the LFW batch size'
-    nrof_batches = nrof_images // batch_size
-    emb_array = np.zeros((nrof_images, embedding_size))
-    lab_array = np.zeros((nrof_images,))
-    for i in range(nrof_batches):
-        feed_dict = {phase_train_placeholder:False, batch_size_placeholder:batch_size}
-        emb, lab = sess.run([embeddings, labels], feed_dict=feed_dict)
-        lab_array[lab] = lab
-        emb_array[lab, :] = emb
-        if i % 10 == 9:
-            print('.', end='')
-            sys.stdout.flush()
-    print('')
-    embeddings = np.zeros((nrof_embeddings, embedding_size*nrof_flips))
-    if use_flipped_images:
-        # Concatenate embeddings for flipped and non flipped version of the images
-        embeddings[:,:embedding_size] = emb_array[0::2,:]
-        embeddings[:,embedding_size:] = emb_array[1::2,:]
-    else:
-        embeddings = emb_array
-
-    assert np.array_equal(lab_array, np.arange(nrof_images))==True, 'Wrong labels used for evaluation, possibly caused by training examples left in the input pipeline'
-    tpr, fpr, accuracy, val, val_std, far = lfw.evaluate(embeddings, actual_issame, nrof_folds=nrof_folds, distance_metric=distance_metric, subtract_mean=subtract_mean)
-    
-    print('Accuracy: %2.5f+-%2.5f' % (np.mean(accuracy), np.std(accuracy)))
-    print('Validation rate: %2.5f+-%2.5f @ FAR=%2.5f' % (val, val_std, far))
-    
-    auc = metrics.auc(fpr, tpr)
-    print('Area Under Curve (AUC): %1.3f' % auc)
-    eer = brentq(lambda x: 1. - x - interpolate.interp1d(fpr, tpr)(x), 0., 1.)
-    print('Equal Error Rate (EER): %1.3f' % eer)
-
-
-def closest_number(number, divisible, max_bound):
-    for i in range(max_bound+1):
-        if number % (divisible + i) == 0:
-            return divisible + i
-        if number % (divisible - i) == 0:
-            return divisible - i
-
-    return None
 
 if __name__ == '__main__':
     # main(parse_arguments(sys.argv[1:]))
